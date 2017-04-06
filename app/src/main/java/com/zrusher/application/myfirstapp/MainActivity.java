7d1eea7c84242689e3e7f9b33610b054d2fc59ac
rusher <rusher315@sina.com>
2017-04-06 18:17:08 +0800

    2 commit

--- app/src/main/java/com/zrusher/application/myfirstapp/MainActivity.java
+++ app/src/main/java/com/zrusher/application/myfirstapp/MainActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,15 +16,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //
+        Button button_toast  = (Button) findViewById(R.id.toast_button);
+        button_toast.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                String message = getEditText();
+                //
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     /** Called when the user clicks the Send button */
-    public void sendMessage(View view) {
+    public void sendMessage(View view)
+    {
+        String message = getEditText();
+        //
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+    public String getEditText()
+    {
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        return editText.getText().toString();
     }
 
 }
